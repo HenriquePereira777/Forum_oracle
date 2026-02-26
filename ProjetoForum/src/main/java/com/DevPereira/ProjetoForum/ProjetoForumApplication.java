bde78b5da681e466a296a32e8db8704f3b5cdb48
PereiraDev <henrique777pereiraa@gmail.com>
2026-02-26 17:04:09 -0300

    iniciando projeto

---
+++ ProjetoForum/src/main/java/com/DevPereira/ProjetoForum/ProjetoForumApplication.java
@@ -0,0 +1,13 @@
+package com.DevPereira.ProjetoForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoForumApplication.class, args);
+	}
+
+}
---
+++ ProjetoForum/src/test/java/com/DevPereira/ProjetoForum/ProjetoForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevPereira.ProjetoForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
